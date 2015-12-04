6fe83592e9b8df3db9abc6afdb86c2edb32f7af5
biousco <441936245@qq.com>
2015-12-04 18:22:28 +0800

    用promise改写

--- routes/crawler.js
+++ routes/crawler.js
@@ -1,59 +1,50 @@
 var express = require('express');
 var router = express.Router();
-var url = require('url');
-var superagent = require('superagent');
-var cheerio = require('cheerio');
 var eventproxy = require('eventproxy');
-var crawlerLib = require('../lib/crawler.js');
+var crawlerLib = require('../lib/crawler_q.js');
+var DOUBAN = require('../lib/douban.js');
 
-/* crawler home page. */
-router.get('/', function(req, res, next) {
+router.get('/a', function (req, res, next) {
 	var render_res = res;
 	var bookList = [];
 	var bookUrllist;
 	var count = 0;
 
-	crawlerLib.getBookListUrl(function (result) {
-		bookUrllist = result;
-		var ep = new eventproxy();
-
-		ep.after('craw_finished', bookUrllist.length, function () {
-			render_res.render('crawler', { 
-				title: 'Crawler',
-				bookList: bookList
-			});
+	crawlerLib.getUrlContent(DOUBAN.index)
+		.then(function (content) {
+			return crawlerLib.getBookListUrl(content);
 		})
+		.then(function (result) {
+			bookUrllist = result;
+			var ep = new eventproxy();
 
-		bookUrllist.forEach(function (bookurl) {
-			crawlerLib.getUrlContent(bookurl,function (abook) {
-				var book = crawlerLib.getBookInfo(abook);
-				bookList.push(book);
-				console.log("has finished: " + count + " total: " + bookUrllist.length);
-				ep.emit('craw_finished');
-				count++;
+			ep.after('craw_finished', bookUrllist.length, function () {
+				render_res.render('crawler', { 
+					title: 'Crawler',
+					bookList: bookList
+				});
+			})
+
+			bookUrllist.forEach(function (bookurl) {
+				crawlerLib.getUrlContent(bookurl).then(function (abook) {
+					var book = crawlerLib.getBookInfo(abook);
+					console.log(book);
+					bookList.push(book);
+					console.log("has finished: " + count + " total: " + bookUrllist.length);
+					ep.emit('craw_finished');
+					count++;
+				})
 			})
 		})
-
-		// for(var i = 0; i < bookUrllist.length; i++) {
-		// 	crawlerLib.getUrlContent(bookUrllist[i],function (abook) {
-
-		// 		count++;
-				
-		// 	})
-		// }
-
-		// var timer = setInterval(function () {
-		// 	if(count < bookUrllist.length) return;
-		// 	console.log('yep!');
-
-		// 	clearInterval(timer);
-		// },2000);
-
-	});
+})
 
 
-	
 
-});
+
+
+
+
+
+
 
 module.exports = router;

51e19356ea8e45a463faef7460d3cf7e56845d4a
Biousco <441936245@qq.com>
2016-01-04 22:32:18 +0800

    添加bootstrap样式

--- routes/crawler.js
+++ routes/crawler.js
@@ -6,11 +6,13 @@ var DOUBAN = require('../lib/douban.js');
 var server = require('http').Server(express());
 var io = require('socket.io')(server);
 
-router.get('/a', function (req, res, next) {
+router.get('/index', function (req, res) {
 	var render_res = res;
 	var bookList = [];
 	var bookUrllist;
 	var count = 0;
+	var total = 5,
+		_total = 5;
 
 	crawlerLib.getUrlContent(DOUBAN.index)
 		.then(function (content) {
@@ -20,19 +22,21 @@ router.get('/a', function (req, res, next) {
 			bookUrllist = result;
 			var ep = new eventproxy();
 
-			ep.after('craw_finished', bookUrllist.length, function () {
+			ep.after('craw_finished', total, function () {
 				render_res.render('crawler', { 
 					title: 'Crawler',
 					bookList: bookList
 				});
-			})
+			});
 
 			bookUrllist.forEach(function (bookurl) {
+				if(_total < 0) return;
+				_total--;
 				crawlerLib.getUrlContent(bookurl).then(function (abook) {
 					var book = crawlerLib.getBookInfo(abook);
 					console.log(book);
 					bookList.push(book);
-					console.log("has finished: " + count + " total: " + bookUrllist.length);
+					console.log("has finished: " + count + " total: " + total);
 					ep.emit('craw_finished');
 					console.log('Start append to file ' + DOUBAN.resultFile + '...');
 					crawlerLib.appendFile(DOUBAN.resultFile, crawlerLib.generateStr(book));
@@ -40,14 +44,14 @@ router.get('/a', function (req, res, next) {
 				})
 			})
 		})
-})
+});
 
 io.on('connection', function (socket) {
 	socket.emit('news', { hello: 'world'});
 	socket.on('my other event', function (data) {
 		console.log(data);
 	})
-})
+});
 
 
 

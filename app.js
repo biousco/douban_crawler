f7d3141d2a1626d363bddbc6f45b11668a08989c
Biousco <441936245@qq.com>
2016-01-04 23:14:18 +0800

    添加知乎

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users'),
-	crawler = require('./routes/crawler');
+	crawler = require('./routes/crawler'),
+    zhihu = require('./routes/zhihu');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/crawler', crawler);
+app.use('/zhihu', zhihu);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
